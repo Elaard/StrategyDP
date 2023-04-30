b5e713c7801701c0555173675b94efd223fc0c18
Elaard <bartek.dv.89@gmail.com>
2023-04-30 16:17:40 +0200

    feat: add description

--- DP_calculator/Program.cs
+++ DP_calculator/Program.cs
@@ -4,6 +4,11 @@ namespace DP_calculator
 {
     class Program
     {
+        //The Strategy design pattern defines a family of algorithms, encapsulate each one, and make them interchangeable.
+        //This pattern lets the algorithm vary independently from clients that use it.
+
+        //It is worth using this pattern when you have many similar classes in the program,
+        //differing only in the way they perform some tasks.
         static void Main(string[] args)
         {
             TemperatureStrategy.Use();
